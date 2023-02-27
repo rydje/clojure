b2dc10f293682a45bb8cc717a7408f84ca2358d6
Bobby Towers <porkostomus@mail.com>
2023-02-27 15:41:51 -0800

    init new exercise

---
+++ exercises/concept/elyses-transformative-enchantments/project.clj
@@ -0,0 +1,4 @@
+(defproject elyses-transformative-enchantments "0.1.0-SNAPSHOT"
+  :description "elyses-transformative-enchantments exercise."
+  :url "https://github.com/exercism/clojure/tree/main/exercises/concept/elyses-transformative-enchantments"
+  :dependencies [[org.clojure/clojure "1.10.0"]])
